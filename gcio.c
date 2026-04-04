aa00ec15d6950c3ebe0e71d73e6c49d87639f028
Brette <98058652+brette-0@users.noreply.github.com>
2026-04-04 03:29:54 +0100

    Changes to support latest changes

--- gcio.c
+++ gcio.c
@@ -4,26 +4,14 @@
 #define GCIO_ACK __asm__("bit $4016")
 namespace gcio {
     enum TASKS : uint8_t {
+        LEGACY,
         REPORT,
         BEHAVE,
         INMASK,
         INVERT,
         RUMBLE,
+        LSETUP,
         END
-    }
-
-    inline static void __Restore(const TASKS task) 
-        __attribute__(
-                (diagnose_if(!__builtin_constant_p(task), "task must be constant", "error"))
-        )
-        __attribute__(
-                (diagnose_if(task >= END, "task must be less than END", "error"))
-        )
-        {
-        #pragma unroll(END - task + 1)
-        for (uint8_t i = 0; i < END - task + 1; i++){
-            GCIO_ACK;
-        }
     }
 
     inline static void __SwitchTask(const TASKS task)
@@ -47,8 +35,6 @@ namespace gcio {
             *(volatile uint8_t*)0x4016 = behave >> i;
             GCIO_ACK;
         }
-
-        __RestoreTask(BEHAVE);
     }
 
     inline static void __WriteQWord(const uint8_t* qword){
@@ -61,12 +47,10 @@ namespace gcio {
     void ChangeMask(const uint8_t* mask){
         __SwitchTask(INMASK);
         __WriteQWord(mask);
-        __Restore(INMASK);
     }
 
     void ChangeInvert(const uint8_t* invert){
         __SwitchInvert(INVERT);
         __WriteQWord(mask);
-        __Restore(INVERT)
     }
 }
