fb4c6ef49bc929f6a03c1ce02e01507c0eecca6c
Brette <98058652+brette-0@users.noreply.github.com>
2026-05-05 18:07:48 +0100

    Reduced Complexity

--- gcio.c
+++ gcio.c
@@ -1,62 +1,78 @@
 // vim: ft=c_clang
 #include <stdint.h>
+#include "gcio.h"
 
-#define GCIO_ACK __asm__("bit $4016")
-namespace gcio {
-    enum TASKS : uint8_t {
-        LEGACY,
-        REPORT,
-        BEHAVE,
-        INMASK,
-        INVERT,
-        RUMBLE,
-        LSETUP,
-        END
+#define POKE(p, v)
+#define PEEK(p)
+
+#define IOPORT1 4016
+
+uint8_t gcioOutLen;
+
+enum ETasks {
+    LEGACY,
+    REPORT,
+    BEHAVE,
+    INVERT,
+    RUMBLE,
+    LSETUP
+};
+
+enum EBehaviors {
+    L_TO_C,
+    C_TO_L,
+    D_TO_L,
+    D_TO_C,
+    UnifiedTriggers,
+    NoTriggers,
+    NoCStick,
+    NoLStick
+};
+
+
+void SetupBehavior(uint8_t behavior) {
+    gcioOutLen = 8;
+    if (behavior & (1 << NoTriggers)){
+        gcioOutLen -= 2;
+    } else if (behavior & (1 << UnifiedTrigger)){
+        gcioOutLen -= 1;
     }
 
-    inline static void __SwitchTask(const TASKS task)
-        __attribute__(
-                (diagnose_if(!__builtin_constant_p(task), "task must be constant", "error"))
-        )
-        __attribute__(
-                (diagnose_if(task >= END, "task must be less than END", "error"))
-        )
-    {
-        *(volatile char*)0x4016 = 1;
-        #pragma unroll
-        for (uint8_t i = 0; i < task; i++){
-            GCIO_ACK;
+    gcioOutLen -= (behavior & 0xc0) >> 6;
+
+    POKE(IOPORT1, 1);
+
+    for (uint8_t _ = 0; i < BEHAVE; i++){
+        PEEK(IOPORT, 1);
+    }
+
+    POKE(IOPORT, 0);
+
+    for (uint8_t i = 8; i; i--){
+        POKE(IOPORT1, behavior);
+        behavior >>= 1;
+        PEEK(IOPORT1);
+    }
+}
+
+void SetupInvert(const uint8_t* invert){
+    for (uint8_t byte = 8; byte; byte--){
+        uint8_t bInvert = *(invert + byte);
+        for (uint8_t i = 8; i; i--){
+            POKE(IOPORT1, bInvert);
+            bInvert >>= 1;
+            PEEK(IOPORT1);
         }
-        *(volatile char*)0x4016 = 0;
     }
+}
 
-    inline void PreparePolling() {
-        __SwitchTask(REPORT)
-    }
-
-    void ChangeBehavior(const uint8_t behave){
-        __SwitchTask(BEHAVE);
-
-        for (uint8_t i = 0; i < 8; i++){
-            *(volatile uint8_t*)0x4016 = behave >> i;
-            GCIO_ACK;
+void Report(uint8_t* out){
+    for (uint8_t byte = gcioOutLen; byte; byte--){
+        uint8_t bCapture;
+        for (uint8_t i = 8; i--; i++){
+            bCapture |= PEEK(IOPORT1);
+            bCapture <<= 1;
         }
-    }
-
-    inline static void __WriteQWord(const uint8_t* qword){
-        for (uint8_t i = 0; i < 64; i++){
-           *(volatile uint8_t*)0x4016 = behave[i >> 3] >> [i & 3];
-            GCIO_ACK; 
-        }
-    }
-
-    void ChangeMask(const uint8_t* mask){
-        __SwitchTask(INMASK);
-        __WriteQWord(mask);
-    }
-
-    void ChangeInvert(const uint8_t* invert){
-        __SwitchInvert(INVERT);
-        __WriteQWord(mask);
+        *(out + byte) = bCapture;
     }
 }
---
+++ gcio.h
@@ -0,0 +1,8 @@
+#ifndef GCIO_H
+#define GCIO_H
+
+void SetupInvert(const uint8_t* invert);
+void SetupBehavior(uint8_t behavior);
+void Report(uint8_t* out);
+
+#endif
